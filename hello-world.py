ae32af58cbf7a5c74febfca079dfa634eeb099a0
Abel1802 <lz980213@163.com>
2020-07-18 22:49:01 +0800

    First assignment with tools installaing

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,10 @@
 ### Write your code here
+
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+if __name__  == "__main__":
+   
+    print("Hello World!") 
+    print("time:", time.strftime('%H:%M:%S',time.localtime(time.time())))
